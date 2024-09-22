23aa5b87b9148ac784a88f3900ade97fe714b594
GermanMakarkin <german.makarkin@gmail.com>
2024-09-22 14:14:17 +0300

    add bubbleSort

---
+++ bubbleSort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& v) {
+    int n = v.size();
+    for (int i = 0; i < n - 1; i++) 
+        for (int j = 0; j < n - i - 1; j++) 
+            if (v[j] > v[j + 1])
+                swap(v[j], v[j + 1]);
+}
+
+int main() {
+    vector<int> v = {5, 1, 4, 2, 8};
+
+    // Sorting the vector v
+    bubbleSort(v);
+    for (auto i : v)
+        cout << i << " ";
+    return 0;
+}
